de3df5b278d5dcb464dbfb4547f9957fc6a7c269
belgacem abibakr <belgacemabibakr@gmail.com>
2021-05-30 16:23:59 +0100

    Update ALL

---
+++ app/src/main/java/com/bobabelga/diceroller/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.bobabelga.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bobabelga.diceroller.databinding.ActivityMainBinding
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.rollBtn.setOnClickListener {
+            rollDice()
+        }
+        }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        binding.diceImg.setImageResource(drawableResource)
+    }
+}
